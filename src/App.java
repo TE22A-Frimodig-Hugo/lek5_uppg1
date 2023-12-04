ab65e5613fde0a083a5bff54b3bda0c483ce4a22
Hugo Frimodig <Hugo.Frimodig@elev.ga.ntig.se>
2023-12-04 09:19:26 +0100

    projekt klart

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App { 
+    public static void main(String[] args) throws Exception {
+        Scanner keyboard = new Scanner(System.in);
+        //Skapa ett fält med talen  8,4,6,3,9:
+        int[] fält1 = {8, 4, 6, 3, 9};
+
+        // Skriv ut talen i fälten med en for-loop:
+        for (int i = 0; i < fält1.length; i++)
+        {
+            System.out.print(fält1[i] + " ");
+        }
+
+        System.out.println("Längden av fältet "+ fält1.length);
+        
+
+    }
+}
